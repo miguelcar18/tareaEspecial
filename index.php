cbb260cc35cf57e1825f717f337b83952fe1d720
Miguel Carmona <miguelcar18@gmail.com>
2014-08-15 16:36:02 -0430

    Codigo parsear imagenes

--- index.php
+++ index.php
@@ -15,6 +15,16 @@ function fijo()
     return $result;
 }
 
+function recibe_imagen ($url_origen,$archivo_destino){
+    $mi_curl = curl_init ($url_origen);
+    $fs_archivo = fopen ($archivo_destino, "w+");
+    curl_setopt ($mi_curl, CURLOPT_FILE, $fs_archivo);
+    curl_setopt ($mi_curl, CURLOPT_HEADER, 0);
+    curl_exec ($mi_curl);
+    curl_close ($mi_curl);
+    fclose ($fs_archivo);
+}
+
 $fijo=fijo();
 //Titulo
 preg_match_all('(<strong id="titulo">(.*)</strong>)siU',$fijo, $subject01);
@@ -23,8 +33,9 @@ preg_match_all('(<td id="descripcion">(.*)</td>)siU',$fijo, $subject02);
 //Imagen
 preg_match_all('(<img src="(.*)" width="(.*)" height="(.*)" />)siU',$fijo, $subject03);
 
-
+$direccionUrl="localhost/tareaEspecial/";
 $contador01=count($subject01);
+$carpeta="uploads/";
 /*$contador02=count($subject02)+1;
 $contador03=count($subject03)+1;*/
 
@@ -32,9 +43,13 @@ for($i=0; $i<=$contador01; $i++)
 {
     $titulo= 'Este es el titulo '.$i.': '.$subject01[1][$i].'<br>';
     $descripcion='Este es la descripcion '.$i.': '.$subject02[1][$i].'<br>';
-    $imagen='La ubicacion de la imagen '.$i.' es: '.$subject03[1][$i].'<br><br>';
-    
+    $urlImagen=$direccionUrl.$subject03[1][$i];
+    $contadorUbicarImagen=count(explode("/", $subject03[1][$i]));
+    $ubicarImagen=explode("/", $subject03[1][$i]);
+    $imagenDestino=$carpeta.$ubicarImagen[$contadorUbicarImagen-1];
+    $imagen='La ubicacion de la imagen '.$i.' es: '.$urlImagen.'<br><br>';
+    ini_set('max_execution_time', 300);
+    recibe_imagen($urlImagen,$imagenDestino);
     echo $titulo.$descripcion.$imagen;
 }
-
 ?>
